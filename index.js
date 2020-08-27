523f147992a6c885240db07fc4cdf62f33b89fc8
Patty Gurusavasti <45869766+Jawqpuizz@users.noreply.github.com>
2020-08-27 16:31:27 -0400

    Edited responsive for ipad
    Changed the width of the responsive scale

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Header Section
 // navbar appear white when scroll up
 function checkScroll() {
-  let startY = $(".navbar").height() * 2; //The point where the navbar changes in px
+  let startY = $(".navbar").height(); //The point where the navbar changes in px
 
   if ($(window).scrollTop() > startY) {
     $(".navbar").addClass("scrolled");
@@ -16,6 +16,11 @@ if ($(".navbar").length > 0) {
   });
 }
 
+// navbar, while on Home, toggle black and transparent if click the menu btn
+$(".navbar-toggler").click(function() {
+  $("nav").toggleClass("nav-toggler-black-navbar");
+});
+
 // navbar collapse when click any menu list
 $(".navbar-nav>li>a").on("click", function() {
   $(".navbar-collapse").collapse("hide");
@@ -26,9 +31,26 @@ $("#navbar-menus").on("click", function() {
   $(".navbar-collapse").collapse("hide");
 });
 
-// navbar-brand change color when hover
-$(".navbar-brand").hover(function() {
-  $(this).css("color", "#ffffff");
-}, function() {
-  $(this).css("color", "#ffcd32");
+//ABOUT SECTION
+//bullet rotate when hover
+$(".btn-xl").click(function() {
+  $(this).toggleClass("btn-xl-rotate");
 });
+
+if ($(window).width() > 750) {
+  // navbar-brand change color when hover
+  $(".navbar-brand").hover(function() {
+    $(this).css("color", "#ffffff");
+  }, function() {
+    $(this).css("color", "#ffcd32");
+  });
+
+  //TEAM SECTION
+  $(".zoom").hover(function() {
+    $(".zoom-img", this).addClass("zoom-img-animate");
+    $(".btn-group", this).addClass("btn-group-animate");
+  }, function() {
+    $(".zoom-img", this).removeClass("zoom-img-animate");
+    $(".btn-group", this).removeClass("btn-group-animate");
+  });
+}
