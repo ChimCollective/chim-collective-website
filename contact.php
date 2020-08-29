890b4329d2d7ae0ec500739d0ed539dd4bda8e2c
wannakornGG <70292379+wannakornGG@users.noreply.github.com>
2020-08-28 21:43:36 -0400

    send email section
    hi n'fai ,i think this is the easiest option to be able to send an email.I create php file  as attached that is link with yr html file.In html file,can you take a look at line 352 .I need to add that line to html but I m not really sure where I suppose to add the ending </form>  .For php file,im pretty sure it is right since it s the most popular way in youtube.
    
    Thank ka

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php
+
+<?php
+  $name=$_POST['name'];
+  $visitor_email=$_POST['email'];
+  $message=$_POST['msg'];
+
+  $email_subject="New Form Submission";
+  $email_body="User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "User Message: $message.\n";
+
+  $to=sungthongngamkik@gmail.com;
+  $headers .=Reply-TO: $visitor_email \r\n";
+  mail($to,$email_subject,$email_body,$headers);
+
+  header("Location: index.html");
+
+
+ ?>
