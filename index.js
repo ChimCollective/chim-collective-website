d59c7eedfaa351c4d4ec06281e1bc46a570c3f94
Pikulkaew Boonpeng <51871643+PKboon@users.noreply.github.com>
2020-09-08 22:26:25 -0400

    Re-upload master

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
+setInterval(function() {
+  let pic1 = "images/" + (Math.floor(Math.random() * 10) + 1) + ".jpg";
+  let pic6 = "images/" + (Math.floor(Math.random() * 10) + 1) + ".jpg";
+  let pic8 = "images/" + (Math.floor(Math.random() * 10) + 1) + ".jpg";
+  let pic9 = "images/" + (Math.floor(Math.random() * 10) + 1) + ".jpg";
+  let pic15 = "images/" + (Math.floor(Math.random() * 10) + 1) + ".jpg";
+
+	$("#pic1").fadeTo(400, 0).attr("src", pic1).fadeTo(400, 1).delay(1000).fadeTo(400, 0);
+  $("#pic6").fadeTo(400, 0).attr("src", pic6).fadeTo(400, 1).delay(1000).fadeTo(400, 0);
+	$("#pic8").fadeTo(400, 0).attr("src", pic8).fadeTo(400, 1).delay(1000).fadeTo(400, 0);
+	$("#pic9").fadeTo(400, 0).attr("src", pic9).fadeTo(400, 1).delay(1000).fadeTo(400, 0);
+	$("#pic15").fadeTo(400, 0).attr("src", pic15).fadeTo(400, 1).delay(1000).fadeTo(400, 0);
+},2300);
+
 // Navigation Bar Section
-$(".menu-btn").click(function() {
-  $(this).toggleClass("menu-btn-rotate");
-  $(".menu-list").toggleClass("menu-list-slide");
-});
-
-$(".panel-heading").parent('.panel').hover(function() {
-  $(this).children(".collapse").collapse("show");
+$(".menu-btn, .menu-list").hover(function() {
+  $(".menu-list").addClass("slide");
+  $(".feather-plus").addClass("rotate");
 }, function() {
-  $(this).children(".collapse").collapse("hide");
-});
-
-$(".team-a").hover(function() {
-  $(".team-a").css("color", "white");
-}, function() {
-  $(".team-a").css("color", "black");
-});
-
-$(".panel-body").children('a').hover(function() {
-  $(".team-a").css("color", "white");
-}, function() {
-  $(".team-a").css("color", "black");
+  $(".menu-list").removeClass("slide");
+  $(".feather-plus").removeClass("rotate");
 });
 
 if ($(window).width() > 768) {
