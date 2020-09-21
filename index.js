daa7bf1cbe245ba20b16bf3e737ea12c9902dd95
Pikulkaew Boonpeng <51871643+PKboon@users.noreply.github.com>
2020-09-21 19:09:35 -0400

    Update Home Section

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
-// Yellow Head Box
-$(".yellow-head-box").on("click", function() {
-  $(this).css("scaleX", "140px");
-});
-
 // Team Section
-var rand1 = Math.floor(Math.random() * 10) + 1;
-var rand6 = Math.floor(Math.random() * 10) + 1;
-var rand8 = Math.floor(Math.random() * 10) + 1;
-var rand9 = Math.floor(Math.random() * 10) + 1;
-var rand15 = Math.floor(Math.random() * 10) + 1;
+var teamPics = ["f-1", "f-2", "f-3",
+                "me-1", "me-2", "me-3", "me-4", "me-5", "me-6", "me-7",
+                "mk-1", "mk-2", "mk-3", "mk-4",
+                "o-1", "o-2", "o-3", "o-4", "o-5",
+                "s-1", "s-2", "s-3", "s-4", "s-5"];
+
+var rand1 = teamPics[Math.floor(Math.random() * teamPics.length)];
+var rand6 = teamPics[Math.floor(Math.random() * teamPics.length)];
+var rand8 = teamPics[Math.floor(Math.random() * teamPics.length)];
+var rand9 = teamPics[Math.floor(Math.random() * teamPics.length)];
+var rand15 = teamPics[Math.floor(Math.random() * teamPics.length)];
 
 while (rand6 === rand1) {
-  rand6 = Math.floor(Math.random() * 10) + 1;
+  rand6 = teamPics[Math.floor(Math.random() * teamPics.length)];
 }
 
 while (rand8 === rand6 || rand8 === rand1) {
-  rand8 = Math.floor(Math.random() * 10) + 1;
+  rand8 = teamPics[Math.floor(Math.random() * teamPics.length)];
 }
 
 while (rand9 === rand8 || rand9 === rand6 || rand9 === rand1) {
-  rand9 = Math.floor(Math.random() * 10) + 1;
+  rand9 = teamPics[Math.floor(Math.random() * teamPics.length)];
 }
 
 while (rand15 === rand9 || rand15 === rand8 || rand15 === rand6 || rand15 === rand1) {
-  rand15 = Math.floor(Math.random() * 10) + 1;
+  rand15 = teamPics[Math.floor(Math.random() * teamPics.length)];
 }
 
-var pic1 = "images/" + rand1 + ".jpg";
-var pic6 = "images/" + rand6 + ".jpg";
-var pic8 = "images/" + rand8 + ".jpg";
-var pic9 = "images/" + rand9 + ".jpg";
-var pic15 = "images/" + rand15 + ".jpg";
+var pic1 = "assets/teamSection/" + rand1 + ".jpg";
+var pic6 = "assets/teamSection/" + rand6 + ".jpg";
+var pic8 = "assets/teamSection/" + rand8 + ".jpg";
+var pic9 = "assets/teamSection/" + rand9 + ".jpg";
+var pic15 = "assets/teamSection/" + rand15 + ".jpg";
 
 $("#pic1").attr("src", pic1);
 $("#pic6").attr("src", pic6);
@@ -45,37 +46,37 @@ setInterval(function() {
   var prevRand9 = rand9;
   var prevRand15 = rand15;
 
-  rand1 = Math.floor(Math.random() * 10) + 1;
-  rand6 = Math.floor(Math.random() * 10) + 1;
-  rand8 = Math.floor(Math.random() * 10) + 1;
-  rand9 = Math.floor(Math.random() * 10) + 1;
-  rand15 = Math.floor(Math.random() * 10) + 1;
+  rand1 = teamPics[Math.floor(Math.random() * teamPics.length)];
+  rand6 = teamPics[Math.floor(Math.random() * teamPics.length)];
+  rand8 = teamPics[Math.floor(Math.random() * teamPics.length)];
+  rand9 = teamPics[Math.floor(Math.random() * teamPics.length)];
+  rand15 = teamPics[Math.floor(Math.random() * teamPics.length)];
 
   while (rand1 === prevRand1) {
-    rand1 = Math.floor(Math.random() * 10) + 1;
+    rand1 = teamPics[Math.floor(Math.random() * teamPics.length)];
   }
 
   while (rand6 === rand1 || rand6 === prevRand6) {
-    rand6 = Math.floor(Math.random() * 10) + 1;
+    rand6 = teamPics[Math.floor(Math.random() * teamPics.length)];
   }
 
   while (rand8 === rand6 || rand8 === rand1 || rand8 === prevRand8) {
-    rand8 = Math.floor(Math.random() * 10) + 1;
+    rand8 = teamPics[Math.floor(Math.random() * teamPics.length)];
   }
 
   while (rand9 === rand8 || rand9 === rand6 || rand9 === rand1 || rand9 === prevRand9) {
-    rand9 = Math.floor(Math.random() * 10) + 1;
+    rand9 = teamPics[Math.floor(Math.random() * teamPics.length)];
   }
 
   while (rand15 === rand9 || rand15 === rand8 || rand15 === rand6 || rand15 === rand1 || rand15 === prevRand15) {
-    rand15 = Math.floor(Math.random() * 10) + 1;
+    rand15 = teamPics[Math.floor(Math.random() * teamPics.length)];
   }
 
-  pic1 = "images/" + rand1 + ".jpg";
-  pic6 = "images/" + rand6 + ".jpg";
-  pic8 = "images/" + rand8 + ".jpg";
-  pic9 = "images/" + rand9 + ".jpg";
-  pic15 = "images/" + rand15 + ".jpg";
+  pic1 = "assets/teamSection/" + rand1 + ".jpg";
+  pic6 = "assets/teamSection/" + rand6 + ".jpg";
+  pic8 = "assets/teamSection/" + rand8 + ".jpg";
+  pic9 = "assets/teamSection/" + rand9 + ".jpg";
+  pic15 = "assets/teamSection/" + rand15 + ".jpg";
 
   $("#pic1").attr("src", pic1);
   $("#pic6").attr("src", pic6);
