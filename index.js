19930db7bc3e95b301b134e6739bafea42c2d8cb
Pikulkaew <pkboonpeng@gmail.com>
2020-11-03 10:15:29 -0500

    nav-bar collaspe when window is clicked

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// // navbar appear white when scroll up
-// function checkScroll() {
-//   let startY = $(".navbar").height(); //The point where the navbar changes in px
-//
-//   if ($(window).scrollTop() > startY) {
-//     $(".navbar").addClass("scrolled");
-//   } else {
-//     $(".navbar").removeClass("scrolled");
-//   }
-// }
-// // navbar appear white when scroll up
-// if ($(".navbar").length > 0) {
-//   $(window).on("scroll load resize", function() {
-//     checkScroll();
-//   });
-// }
-//
-// // navbar, while on Home, toggle black and transparent if click the menu btn
-// $(".navbar-toggler").click(function() {
-//   $("nav").toggleClass("nav-toggler-black-navbar");
-// });
-
 // navbar collapse when click any menu list
 $(".navbar-nav>li>a").on("click", function() {
   $(".navbar-collapse").collapse("hide");
