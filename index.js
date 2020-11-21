f72c85fd75534bc90564605a8ed8e9c647bc8d20
Pikulkaew <pkboonpeng@gmail.com>
2020-11-21 12:36:38 -0500

    update team img, remove team bg use js for hovering the team imgs

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ $(".navbar-nav>li>a").on("click", function() {
 $(window).on("click", function() {
   $(".navbar-collapse").collapse("hide");
 });
+
+$(".card-img-top").hover(function() {
+  let imgNameBw = $(this).attr('src');
+  let imgNameCl = imgNameBw.replace("bw/bw-", "cl/");
+  $(this).attr('src', imgNameCl);
+}, function() {
+  let imgNameCl = $(this).attr('src');
+  let imgNameBw = imgNameCl.replace("cl/", "bw/bw-");
+  $(this).attr('src', imgNameBw);
+});
